b68079608f2985720c207c668463e02fb76d384a
Mani Krishna Karri <manikrishnakarri@gmail.com>
2026-03-11 23:19:45 +0530

    Call Stack and Event Loops

--- day4.js
+++ day4.js
@@ -75,13 +75,29 @@ person.greet(); // Output: Hello, my name is Mani Krishna
 
 const person1 = {
     name : "Sathwik",
-    greet : ()=> console.log(this.name);
+    greet : ()=> console.log(this.name)
 }
 const person2 = {
     name : "Mani Krishna"
 }
 //person2.greet = person1.greet;
-person2.greet(); // Output: undefined
+//person2.greet(); // Output: undefined
 
 // In the above example, the arrow function does not have its own this context, so it inherits this from the enclosing scope, which is the global scope. Since there is no name property in the global scope, this.name evaluates to undefined.
 
+// Call Stack is a data structure that keeps track of the function calls in a program. It follows the Last In, First Out (LIFO) principle, meaning that the most recently called function is the first one to be executed. When a function is called, it is added to the top of the call stack, and when it returns, it is removed from the stack. This allows JavaScript to manage function execution and handle nested function calls effectively.
+
+// Event Loop is a mechanism that allows JavaScript to perform non-blocking 
+// operations by offloading tasks to the browser's event handling system. 
+// It continuously checks the call stack and the task queue. 
+// If the call stack is empty, it takes the first task from the task queue and 
+// pushes it onto the call stack for execution. 
+// This allows JavaScript to handle asynchronous operations, such as user interactions, network requests, and timers, without blocking the main thread of execution.
+
+console.log("Start");
+setTimeout(() => {
+    console.log("Inside setTimeout");
+}, 3000);
+console.log("End");
+
+//Microtasks and Macrotasks
